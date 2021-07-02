125c3ae3023e62720256a79a3ac4fc796aeb386c
GiHuLeo385 <86833012+GiHuLeo385@users.noreply.github.com>
2021-07-02 20:13:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import string
-import random
+import string #To get the letter and symbol list
+import random #Very useful randomizer functions
 
 print("This programme will generate a randomized password")
 Letter = int(input("Enter the number of letter you want: "))
@@ -8,7 +8,7 @@ Symbol = int(input("Enter the number of symbol you want: "))
 
 print("The length of your password is: ", Letter + Number + Symbol)
 
-Password = []
+Password = [] #Defining the Password "set" beforehand
 
 x = range(Letter)
 for n in x:
@@ -16,12 +16,12 @@ for n in x:
 
 x = range(Number)
 for n in x:
-    Password.append(str(random.randint(0, 9)))
+    Password.append(str(random.randint(0, 9)))  #I use the function str() to convert randomized numbers into string for later
 
 x = range(Symbol)
 for n in x:
     Password.append(random.choice(string.punctuation))
 
-random.shuffle(Password)
+random.shuffle(Password) #I was thinking of combining this with the below command, but it turns out random.shuffle() returns None rather than a list
 
 print("".join(Password))
